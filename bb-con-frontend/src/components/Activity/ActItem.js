730dac7034ae8bae3d2ad65cb74507dbd7478738
Jin Linder <jinlinder@hotmail.com>
2022-03-29 20:19:15 +0200

    fixed show single item

--- bb-con-frontend/src/components/Activity/ActItem.js
+++ bb-con-frontend/src/components/Activity/ActItem.js
@@ -1,19 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom'
+import JoinAct from './JoinAct';
 
 export default function ActItem() {
 
+    const [item, setItem] = useState([])
     const params = useParams();
     console.log(params.actId);
+    const url = `http://localhost:5000/activity/item/${params.actId}`
+
+    //get the activity with activity Id
     useEffect(()=>{
-        fetch(`http://localhost:5000/activity/item/${params.actId}`)
+        fetch(url)
             .then(res=>res.json())
-            .then(data=>{console.log(data); console.log("fetch works")} )
-    }       
-    )
+            .then(data=>{console.log(data); setItem(data)}  )
+    },
+    [url])
   return (
     <div>
-        <h3>Title:</h3>
+        <h3>Activity</h3>
+        {item.map((i)=>( <div>
+          <p>Title:{i.title}</p>
+          <p>time:{i.time}</p>
+          <p>community:{i.community}</p>
+          <p>adress:{i.adress}</p>
+          <p>info:{i.info}</p>
+          <p>participants:{i.participants}</p>
+          <button>Join</button>
+        </div>
+          
+          
+        ))}
+    
     </div>
   )
 }
---
+++ bb-con-frontend/src/components/Activity/JoinAct.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function JoinAct(props) {
+  console.log(props);
+  return (
+    <div>title:{ props.i.title}</div>
+  )
+}
