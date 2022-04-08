f39b23bbee2193d04d8a22f914bf9ec23b04061f
Jin Linder <jinlinder@hotmail.com>
2022-04-08 16:49:29 +0200

    fixed like issue in Comment component

--- bb-con-frontend/src/components/Community/Comments.js
+++ bb-con-frontend/src/components/Community/Comments.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState,useEffect } from 'react';
 
 
+import { FaThumbsUp, FaComment } from 'react-icons/fa'
 export default function Comments(props) {
     const user = localStorage.getItem("user");
     const [inputValue, setInputValue] = useState("");
@@ -9,15 +10,16 @@ export default function Comments(props) {
     const [likes, setLikes] = useState(props.item.likes)
     // const [clicked, setClicked] = useState(false)
     const [likeElement, setLikeElement] = useState([])
-    const result = likes.find(like=>{return like===user})
+    const result = likes.find(like=>{return like===user});
+    const likesNummber = likes.length
     console.log(result)
     
     useEffect(()=>{
       if(result){
-        setLikeElement([<button onClick={unLike}>Unlike</button>]);
+        setLikeElement([<FaThumbsUp onClick={unLike} style = {{color:"blue"}}/>]);
         
       } else if (!result) {
-        setLikeElement([<button onClick={like}>like</button>]);
+        setLikeElement([ <FaThumbsUp onClick={like} style = {{color:"grey"}}/>]);
         
       }
     }, [likes])
@@ -76,11 +78,10 @@ export default function Comments(props) {
     )
     
 
-
   return (
     <div>
-            {likeElement}
-            <a href="#comment">comment</a>
+            <div>{likeElement} {likesNummber}   <a href="#comment">comment</a> <FaComment/></div> 
+           
             <p>Comments:{comments.map((com)=>(<div><p>user: {com.user}</p>
             <p>text: {com.text}</p></div>))}</p>
             <input type="text" 
@@ -90,20 +91,3 @@ export default function Comments(props) {
     </div>
   )
 }
- //handle like function
-    // const likePost = () => {
-    //   setClicked(!clicked);
-    //   const result = likes.find(like=>{return like===user});
-    //   if(clicked===true & !result) {
-    //     setLikes([...likes, user])
-    //   } else if (clicked===false & result) {
-    //     const filterResult = likes.filter(like=>{return like !==user});
-    //     setLikes(filterResult)
-    //   }
-      
-    //   // if(likes.include(user)===false) {
-    //   //   setLikes([...likes, user])
-    //   // }
-    //   console.log("like function")
-    //   console.log(result)
-    // }
